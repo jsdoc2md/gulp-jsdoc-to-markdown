8c76b1207bf9c34134c98af39d9765a486199dc5
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-07-19 12:52:24 +0100

    buffer mode working

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 "use strict";
 var gulp = require("gulp");
 var gulpJsdoc2md = require("./");
+var rename = require("gulp-rename");
 
 gulp.task("default", function(){
     gulp.src("test/fixture/code.js")
         .pipe(gulpJsdoc2md())
-        .pipe(gulp.dest("tmp"));
+        .pipe(rename("code.md"))
+        .pipe(gulp.dest("tmp/one"));
+        
+    gulp.src("test/fixture/*.js")
+        .pipe(gulpJsdoc2md())
+        .pipe(rename(function(path){
+            path.extname = ".md";
+        }))
+        .pipe(gulp.dest("tmp/two"));
+    
 });
