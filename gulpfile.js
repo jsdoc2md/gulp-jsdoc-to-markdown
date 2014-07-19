e4bf93cf9cc33926686ad51a603a53513bdf0217
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-07-19 14:56:40 +0100

    streaming mode

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,36 @@
 var gulp = require("gulp");
 var gulpJsdoc2md = require("./");
 var rename = require("gulp-rename");
+var del = require("del");
 
-gulp.task("default", function(){
-    gulp.src("test/fixture/code.js")
+gulp.task("clean", function(done){
+    del("tmp", done);
+});
+
+gulp.task("one", [ "clean" ], function(){
+    return gulp.src("test/fixture/code.js")
         .pipe(gulpJsdoc2md())
         .pipe(rename("code.md"))
         .pipe(gulp.dest("tmp/one"));
-        
-    gulp.src("test/fixture/*.js")
+});
+
+gulp.task("two", [ "clean" ], function(){
+    return gulp.src("test/fixture/*.js")
         .pipe(gulpJsdoc2md())
         .pipe(rename(function(path){
             path.extname = ".md";
         }))
-        .pipe(gulp.dest("tmp/two"));
-    
+        .pipe(gulp.dest("tmp/two"));    
 });
+
+/* streaming mode */
+gulp.task("three", [ "clean" ], function(){
+    return gulp.src("test/fixture/*.js", { buffer: false })
+        .pipe(gulpJsdoc2md())
+        .pipe(rename(function(path){
+            path.extname = ".md";
+        }))
+        .pipe(gulp.dest("tmp/three"));    
+});
+
+gulp.task("default", [ "clean", "one", "two", "three" ]);
