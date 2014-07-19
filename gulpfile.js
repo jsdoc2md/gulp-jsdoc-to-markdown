5168b7b2ec985bbb53c4e24582e736038133018b
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-07-19 20:44:10 +0100

    tests

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gutil = require("gulp-util");
 var jsdoc2md = require("./");
 var rename = require("gulp-rename");
 var del = require("del");
+var concat = require("gulp-concat");
 
 gulp.task("clean", function(done){
     del("tmp", done);
@@ -44,4 +45,15 @@ gulp.task("three", [ "clean" ], function(){
         .pipe(gulp.dest("tmp/three"));    
 });
 
-gulp.task("default", [ "clean", "one", "two", "three" ]);
+
+gulp.task("four", [ "clean" ], function() {
+    return gulp.src("test/fixture/*.js")
+        .pipe(concat("all.md"))
+        .pipe(jsdoc2md())
+        .on("error", function(err){
+            gutil.log("jsdoc2md failed:", err.message);
+        })
+        .pipe(gulp.dest("tmp/four"));
+});
+
+gulp.task("default", [ "clean", "one", "two", "three", "four" ]);
--- test/test.js
+++ test/test.js
@@ -1,6 +1,38 @@
+var es = require("event-stream");
+var File = require("vinyl");
 var test = require("tape");
-var lib = require("../");
+var gjsdoc2md = require("../");
+var fs = require("fs");
 
-test("first", function(t){
+test("streaming mode", function(t){
+    var fakeFile = new File({
+      contents: fs.createReadStream("test/fixture/code.js")
+    });
     
+    var stream = gjsdoc2md();
+    stream.write(fakeFile);
+    
+    stream.once("data", function(file){
+        t.ok(file.isStream());
+        
+        file.contents.pipe(es.wait(function(err, data) {
+          t.ok(/#something/.test(data));
+          t.end();
+        }));
+    });
+});
+
+test("buffer mode", function(t){
+    var fakeFile = new File({
+      contents: new Buffer("/**\na global\n@type boolean\n@default\n*/\nvar something = true;\n")
+    });
+    
+    var stream = gjsdoc2md();
+    stream.write(fakeFile);
+    
+    stream.once("data", function(file){
+        t.ok(file.isBuffer());
+        t.ok(/#something/.test(file.contents.toString()));
+        t.end();
+    });
 });
